a700e14e7b7fda5ae0b13ed6be60ce3d85cbb075
wch6887164 <wch6887164@163.com>
2024-09-30 00:31:32 +0800

    Enhanced: init

---
+++ learn-git/src/main/java/com/up/cqp/App.java
@@ -0,0 +1,13 @@
+package com.up.cqp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
